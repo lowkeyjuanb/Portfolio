cae999e303631b37cf8eeb754f2525869dc459da
Juan B <this.is.juanb@gmail.com>
2025-02-17 05:56:40 -0800

    homesc fixes

--- portfolio/src/screens/homesc.jsx
+++ portfolio/src/screens/homesc.jsx
@@ -3,18 +3,18 @@ import linkedinSymbol from '../assets/linkedin-symbol.svg'
 import mailSymbol from '../assets/mail-symbol.svg'
 export default function HomeSc() {
 return (
-    <section className="w-full min-h-screen flex items-center justify-center pb-60">
+    <section className="w-full min-h-screen flex items-center justify-center pb-48 px-10">
         <div> 
-            <p class = "text-4xl">
-                Hello, my name is Juan Barrera and...
+            <p class = "text-3xl">
+                Juan Barrera
             </p>
-            <h1 class = "text-8xl">
-                I'M A SOFTWARE DEVELOPER
+            <h1 class = "text-6xl">
+                SOFTWARE DEVELOPER
             </h1>
             <div className="flex gap-4 justify-center mt-8">
-                <img src={mailSymbol} alt="Mail Icon" className="w-12 h-12" />
-                <img src={githubSymbol} alt="Github Icon" className="w-12 h-12" />
-                <img src={linkedinSymbol} alt="Linkedin Icon" className="w-12 h-12" />
+                <img src={mailSymbol} alt="Mail Icon" className="w-9 h-9" />
+                <img src={githubSymbol} alt="Github Icon" className="w-9 h-9" />
+                <img src={linkedinSymbol} alt="Linkedin Icon" className="w-9 h-9" />
             </div>
         </div>
         
