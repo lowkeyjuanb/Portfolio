bac1382024ab571cdbeda80390203393cd63329c
Juan B <this.is.juanb@gmail.com>
2025-02-17 13:03:11 -0800

    homesc fixes; about me page and keyPoint component

--- portfolio/src/screens/about.jsx
+++ portfolio/src/screens/about.jsx
@@ -1,17 +1,30 @@
-
+import AboutKeyPoints from "../components/aboutKeyPoints";
 export default function AboutSc() {
 return (
-    <section className="w-full min-h-screen flex items-center justify-center pb-48 px-10">
-        <div>
-            <h1>About me</h1>
-            <p>
-            My name is Tomasz Gajda, I'm a third year Applied Computer Science student at the AGH University of Science and Technology in Krakow. 
-            I have been learning Front-End technologies for a year and this time was just enough for me to make sure that this is my place in the industry.
-            Membership in the science club developed my design skills, which quickly turned into a new hobby. 
-            I am fluent in English (spoken and written) and intermediate Spanish. Apart from designing and programming websites, 
-            my passion is all kinds of motorsport - from rallies to the very king of motorsport - formula 1.
-            </p>
+    <section className="w-full min-h-screen flex flex-col items-center py-20">
+        <h1 className="text-2xl font-semibold border-4 border-black p-6 text-center">
+            ABOUT ME
+        </h1>
+        <p className="mt-10 text-lg max-w-4xl text-center">
+            Nulla in velit a metus rhoncus tempus. Nulla congue nulla vel sem varius finibus. 
+            Sed ornare sit amet lorem sed viverra. In vel urna quis libero viverra facilisis ut ac est.
+        </p>
+        <svg width="100" height="4" className="m-20">
+        <rect width="100" height="4" fill="black" />
+        </svg>
+        <div className="flex w-full gap-10">
+            <AboutKeyPoints 
+            keyPointName="DEVELOPMENT" 
+            keyPointDesc="I can design the site based on your needs and suggestions. I can also design the site from scratch and consult you during the job." 
+            />
+            <AboutKeyPoints 
+                keyPointName="MAINTENANCE" 
+                keyPointDesc="I can design the site based on your needs and suggestions. I can also design the site from scratch and consult you during the job." 
+            />
         </div>
+        <svg width="100" height="4" className="m-20">
+        <rect width="100" height="4" fill="black" />
+        </svg>
     </section>
 );
 }
--- portfolio/src/screens/homesc.jsx
+++ portfolio/src/screens/homesc.jsx
@@ -3,31 +3,34 @@ import linkedinSymbol from '../assets/linkedin-symbol.svg'
 import mailSymbol from '../assets/mail-symbol.svg'
 export default function HomeSc() {
 return (
-    <section className="w-full min-h-screen flex items-center justify-center pb-48 px-10">
-        <div> 
-            <h2 class = "text-3xl">
-                Juan Barrera
-            </h2>
-            <h1 class = "text-6xl">
-                SOFTWARE DEVELOPER
-            </h1>
-            <div className="flex gap-4 justify-center mt-8">
-                <img src={mailSymbol} alt="Mail Icon" className="w-9 h-9" />
-                <img src={githubSymbol} alt="Github Icon" className="w-9 h-9" />
-                <img src={linkedinSymbol} alt="Linkedin Icon" className="w-9 h-9" />
+    <section className="w-full max-h-full flex">
+        <div className=''> 
+            <div className='my-32'>
+                <h2 class = "text-3xl justify-center">
+                    Juan Barrera
+                </h2>
+                <h1 class = "text-6xl justify-center">
+                    SOFTWARE DEVELOPER
+                </h1>
+                <div className="flex gap-4 justify-center mt-8">
+                    <img src={mailSymbol} alt="Mail Icon" className="w-9 h-9" />
+                    <img src={githubSymbol} alt="Github Icon" className="w-9 h-9" />
+                    <img src={linkedinSymbol} alt="Linkedin Icon" className="w-9 h-9" />
+                </div>
             </div>
-            
-        <h1 className='flex justify-start text-justify'>
+        <div className='bg-black text-white p-10 text-2xl'>
+            <h1 className='flex justify-start text-justify font-bold'>
                 IT BERRIES
-        </h1>
-        <p className='flex justify-start text-justify'>
-        I am one of the members of a scientific club that brings together enthusiasts of the Internet and new technologies. 
-        At ITberries, we design and develop websites and web applications. 
-        Apart from the projects, our biggest undertaking is the Ciemna Strona UX conference, 
-        which we are organizing for the sixth time. Working in a club as a Front-End Developer 
-        definitely developed my skills in group work and working under time pressure. 
-        If you want to know more about us, click the button below! 
-        </p>
+            </h1>
+            <p className='flex justify-start text-justify py-5 font-thin text-base'>
+                I am one of the members of a scientific club that brings together enthusiasts of the Internet and new technologies. 
+                At ITberries, we design and develop websites and web applications. 
+                Apart from the projects, our biggest undertaking is the Ciemna Strona UX conference, 
+                which we are organizing for the sixth time. Working in a club as a Front-End Developer 
+                definitely developed my skills in group work and working under time pressure. 
+                If you want to know more about us, click the button below! 
+            </p>
+        </div>   
         </div>
     </section>
 );
