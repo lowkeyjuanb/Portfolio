50058d76169e02ec34a228e10e0787fb7ceca230
Juan B <this.is.juanb@gmail.com>
2025-02-23 18:00:17 -0800

    porfolio project row

--- portfolio/src/screens/portfolio.jsx
+++ portfolio/src/screens/portfolio.jsx
@@ -4,9 +4,12 @@ export default function PortfolioSc() {
     return (
         <section className="w-full flex flex-col items-center my-10 px-10">
             <TitleSquare titleSquare="PORTFOLIO"/>
-            <div className="flex flex-row gap-6">
-                <div className="bg-red-500 h-20 w-40">
-
+            <div className="flex flex-row gap-6 py-10">
+                <div className="bg-gray-500 h-60 w-96">
+                </div>
+                <div className="bg-gray-500 h-60 w-96">
+                </div>
+                <div className="bg-gray-500 h-60 w-96">
                 </div>
             </div>
         </section>
