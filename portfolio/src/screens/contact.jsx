9d15f18ca9c87a6d293f70d88db2360e7fedcbeb
Juan B <this.is.juanb@gmail.com>
2025-03-12 21:56:06 -0700

    responsive contact section

--- portfolio/src/screens/contact.jsx
+++ portfolio/src/screens/contact.jsx
@@ -21,25 +21,25 @@ export default function Footer() {
   window.addEventListener("scroll", handleScroll);
 
   return (
-    <footer className="w-full py-10 bg-black text-white">
-      <div className="flex flex-row justify-center items-center gap-10">
+    <footer className="flex flex-col w-full py-10 bg-black text-white">
+      <div className="flex flex-row justify-center items-center gap-5 lg:gap-10">
         {/* The return to top button */}
         {scrolling && (
           <button
             onClick={scrollToTop}
-            className="p-3 bg-gradient-to-b from-purple-700 to-blue-900 rounded-full text-white hover:bg-blue-700 transition duration-300"
+            className="flex lg:text-lg text-xs p-3 bg-gradient-to-b from-purple-700 to-blue-900 rounded-full text-white hover:bg-blue-700 transition duration-300"
           >
             ↑ Return to the top ↑
           </button>
         )}
-        <a href="mailto:this.is.juanb@gmail.com" className="text-lg">
+        <a href="mailto:this.is.juanb@gmail.com" className="lg:text-lg text-xs">
           Email
         </a>
         <a
           href="https://www.linkedin.com/in/juandediosbarrera/"
           target="_blank"
           rel="noopener noreferrer"
-          className="text-lg"
+          className="lg:text-lg text-xs"
         >
           LinkedIn
         </a>
@@ -47,7 +47,7 @@ export default function Footer() {
           href="https://github.com/lowkeyjuanb"
           target="_blank"
           rel="noopener noreferrer"
-          className="text-lg"
+          className="lg:text-lg text-xs"
         >
           GitHub
         </a>
